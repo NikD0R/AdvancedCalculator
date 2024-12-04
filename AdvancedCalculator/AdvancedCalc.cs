02fcd9e7bc86f73ca622740c041085826391cab8
Nikita Doroshenko <nikitokd05@gmail.com>
2024-12-04 15:27:02 +0200

    Add file AdvancedCalc

---
+++ AdvancedCalculator/AdvancedCalc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class AdvancedCalc : OrdinaryCalc, IAdvanced
+{
+    public double ConvertDegreesToRadians(double degrees)
+    {
+        if (degrees < 0 || degrees >= 360)
+        {
+            throw new ArgumentOutOfRangeException("Значення градусів повинне бути в межах від 0 до 360.");
+        }
+        return degrees * (Math.PI / 180);
+    }
+}

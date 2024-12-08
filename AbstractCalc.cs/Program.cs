248703518d31b32db202c16be81e60c2400d4dd4
Diakonova <taia.diakonova@gmail.com>
2024-12-08 13:42:49 +0200

    Додала AbstractCalc

---
+++ AbstractCalc.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿// AbstractCalc.cs
+public abstract class AbstractCalc
+{
+    public string Model { get; set; } = "Standard Calculator";
+
+    public string GetModel() => Model;
+
+    public abstract double Add(double a, double b);
+    public abstract double Subtract(double a, double b);
+    public abstract double Multiply(double a, double b);
+    public abstract double Divide(double a, double b);
+}
